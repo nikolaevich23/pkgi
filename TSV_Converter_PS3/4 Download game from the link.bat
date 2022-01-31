87d329d7242563c136d7a63bc3bbfbe909079e49
ErikPshat <erik_pshat@list.ru>
2022-01-31 06:33:58 +0300

    Added SHA-2 (256 bit) hash

--- TSV_Converter_PS3/4 Download game from the link.bat
+++ TSV_Converter_PS3/4 Download game from the link.bat
@@ -1,13 +1,54 @@
 @echo off
+setlocal EnableDelayedExpansion
 set col=nhcolor.exe
 if exist "%ProgramFiles(x86)%" (set wget=wget64.exe) else (set wget=wget32.exe)
-echo Пример ссылки: http://zeus.dl.playstation.net/cdn/UP0017/NPUA30003_00/ioK4HQEme7gxPUvRL2GdkKcQ22m8NSbH18wyykhfRciOKfcS3wRbg8wDi5mV55RqLthom20FQkQoMj2bdmlYMABmssDfnsbDMVP9s.pkg|nhcolor 06
+echo Пример прямой ссылки на CDN-сервер Sony PlayStation:|!col! 09
+echo http://zeus.dl.playstation.net/cdn/UP0017/NPUA30003_00/ioK4HQEme7gxPUvRL2GdkKcQ22m8NSbH18wyykhfRciOKfcS3wRbg8wDi5mV55RqLthom20FQkQoMj2bdmlYMABmssDfnsbDMVP9s.pkg|!col! 06
 echo.
-echo Начало ссылки парсера dokpub: http://getfile.dokpub.com/yandex/get/|nhcolor 06
+echo Для скачивания игры вставьте ранее скопированную ссылку правой кнопкой:|!col! 09
+set /p link=
 echo.
-set /p link=Для скачивания введите ссылку: 
+:: Дампим контрольную сумму скачиваемого PKG
+set start=!TIME:~0,2!:!TIME:~3,2!:!TIME:~6,2!
+!wget! --spider -S --output-file=spider.txt !link!
+for /f "tokens=1,2 delims= " %%A in (spider.txt) do if [%%A]==[X-Agile-Checksum:] set hshg=%%B && set hshg=!hshg: =!
+del /q "spider.txt"
+for %%C in (!link!) do set gnm=%%~nxC
+echo Скачивание PKG игры:|!col! 0A
+echo !gnm!
+echo Поддерживается докачка, вы в любой момент можете закрыть это окно и продолжить снова.|!col! 09
+echo Пожалуйста, подождите...|!col! 09
 echo.
-echo Скачивание игры: %Link%        please wait...|nhcolor 09
+!wget! -c -q --no-cookies --show-progress --user-agent="Mozilla/5.0 (PLAYSTATION 3 4.88) AppleWebKit/531.22.8 (KHTML, like Gecko)" !link!
 echo.
-%wget% -c --no-cookies --user-agent="Mozilla/5.0 (PLAYSTATION 3 4.87) AppleWebKit/531.22.8 (KHTML, like Gecko)" %Link%
+:: Проверяем контрольную сумму
+echo Проверяется контрольная сумма SHA256 скачанной игры, пожалуйста, подождите... !TIME:~0,2!:!TIME:~3,2!:!TIME:~6,2!|!col! 0A
+echo.
+if exist "!gnm!" (
+	CertUtil -hashfile "!gnm!" SHA256 > "SHA256.txt"
+	for %%D in ("!gnm!") do set size=%%~zD
+) else (
+	echo Не найден скачанный PKG игры в этой папке:|!col! EC
+	echo "!gnm!"|!col! 0E
+	pause & exit
+)
+for /f "tokens=* delims=" %%E in (SHA256.txt) do (
+	set /a ng+=1
+	set hash=%%E
+	if !ng!==2 set SHA256=!hash: =!
+)
+if "!SHA256!"=="!hshg!" (
+	echo Контрольная сумма SHA256 совпадает: !SHA256!|!col! 0B
+	echo Размер скачанного PKG соответсвует: !size! байт|!col! 0B
+	echo.
+	echo Готово^^! PKG игры скачан успешно и проверен на битость по контрольной сумме... !TIME:~0,2!:!TIME:~3,2!:!TIME:~6,2!|!col! 0A
+) else (
+	echo Контрольная сумма SHA256 НЕ СОВПАДАЕТ. Пожалуйста, перекачайте игру...|!col! 0C
+	echo Контрольная сумма SHA256 НЕ СОВПАДАЕТ: !SHA256! !TIME:~0,2!:!TIME:~3,2!:!TIME:~6,2!|!col! 0C
+	echo !SHA256!
+	echo !hshg!
+	pause & exit
+)
+del /q "SHA256.txt"
+echo.
 pause
