c52bb89527e4fb122b3e0d4eb084b953ad11fced
nikolaevich23 <in1975@ya.ru>
2022-08-10 08:59:25 +0400

    Update pkgi.h

--- include/pkgi.h
+++ include/pkgi.h
@@ -5,7 +5,7 @@
 #include "pkgi_dialog.h"
 
 #define PKGI_UPDATE_URL     "https://api.github.com/repos/ErikPshat/pkgi/releases/latest"
-#define PKGI_VERSION        "1.2.3.220802"
+#define PKGI_VERSION        "1.2.3.220809"
 
 #define PKGI_BUTTON_SELECT 0x00010000
 #define PKGI_BUTTON_START  0x00080000
