7904d953821bcf1439ec5d372a82790074e68b50
ErikPshat <erik_pshat@list.ru>
2024-11-10 08:42:13 +0300

    Added SHA-2 (256 bit) hash check 241110

--- TSV_Converter_PS3/3 Get the size and SHA256 from the link.bat
+++ TSV_Converter_PS3/3 Get the size and SHA256 from the link.bat
@@ -1,10 +1,25 @@
 @echo off
-set col=nhcolor.exe
+setlocal enabledelayedexpansion
+
+set col=nhcolor
+if exist temp.txt del /q temp.txt
 if exist "%ProgramFiles(x86)%" (set wget=wget64.exe) else (set wget=wget32.exe)
-echo Test SIZE = http://zeus.dl.playstation.net/cdn/JP9000/NPJA90165_00/slssGPWCSVfJVAaktnCrykcYbmqICStbWqFmHRdyMqJymTICudtDSgEKrqxYDebAnhylnCYWjZNeWLmxqctXjDTupXnhzIporaSEF.pkg|nhcolor 06
-set /p link=Для получения размера введите ссылку: 
+echo Test SIZE = http://zeus.dl.playstation.net/cdn/JP9000/NPJA90165_00/slssGPWCSVfJVAaktnCrykcYbmqICStbWqFmHRdyMqJymTICudtDSgEKrqxYDebAnhylnCYWjZNeWLmxqctXjDTupXnhzIporaSEF.pkg|!col! 06
+set link=%1
+if not defined link set /p link=Для получения размера введите ссылку: 
 echo.
-echo Scanning Size for: %link%|nhcolor 0A
+echo Scanning Size for: %link%|!col! 0A
 echo.
-%wget% --spider --server-response %link%
+%wget% --spider --server-response %link% -o temp.txt
+for /f "tokens=2 delims=:" %%A in ('type temp.txt ^|findstr /i /c:"X-Agile-Checksum:"') do set sha=%%A
+for /f "tokens=2 delims=:" %%B in ('type temp.txt ^|findstr /i /c:"Content-Length:"') do set size=%%B
+type temp.txt |more
+echo Length: !size! |!col! 0B
+if defined sha (
+echo SHA256: !sha! |!col! 0B
+)
+if not defined sha (
+echo SHA256:  Отсутствует контрольная сумма SHA256 на сервере.|!col! 0C
+)
+echo.
 pause
